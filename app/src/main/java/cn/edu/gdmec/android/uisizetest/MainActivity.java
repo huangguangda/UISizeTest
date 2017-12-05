12751d39a854b817ac4527e3fd3427fd787b3f61
huangguangda <2397923107@qq.com>
2017-12-05 18:28:52 +0800

    39

--- app/src/main/java/cn/edu/gdmec/android/uisizetest/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/uisizetest/MainActivity.java
@@ -2,6 +2,7 @@ package cn.edu.gdmec.android.uisizetest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+        float xdpi = getResources ().getDisplayMetrics ().xdpi;
+        float ydpi = getResources ().getDisplayMetrics ().ydpi;
+        Log.d ( "MainActivity", "xdpi is" + xdpi );
+        Log.d ( "MainActivity", "ydpi is" + ydpi );
     }
 }
